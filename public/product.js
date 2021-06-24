6817ec191b7fbd1b83b9faf2294783cf808248f2
FusFendragon <fusfendragon@outlook.fr>
2021-06-24 18:18:10 +0200

    Add custom pick

--- public/product.js
+++ public/product.js
@@ -1,7 +1,6 @@
 // Récuperer l'id dans l'URL
 const url = new URL(window.location.href);
 const urlId = url.searchParams.get("id");
-console.log(urlId);
 
 // Trouver une correspondance
 
@@ -9,6 +8,7 @@ fetch("http://localhost:3000/api/teddies")
 	.then((res) => res.json())
 	.then((data) => {
 		let output = "";
+		let custom = "";
 		data.forEach((teddy) => {
 			if (urlId === teddy._id) {
 				output += `
@@ -19,9 +19,25 @@ fetch("http://localhost:3000/api/teddies")
                         <p>${teddy.description}</p>
                         <p>${teddy.price / 100} €</p>
                     </div>
+                    <div class"teddy-custom">
+                    <form method="post" action="">
+                        <p>
+                            <label for="color">Couleur de la lentille</label><br />
+                            <select name="color" id="color">   
+                            </select>
+                        </p>
+                    </form>
+                    </div>
                 </div>
                 `;
+
+				for (let i = 0; i < teddy.colors.length; i++) {
+					custom += `
+                <option value=${teddy.colors[i]}>${teddy.colors[i]}</option>
+                `;
+				}
 			}
 		});
 		document.querySelector("section").innerHTML = output;
+		document.querySelector("select").innerHTML = custom;
 	});
